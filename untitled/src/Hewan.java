33221e0a8385ebb688acdecc74699c6e808a5e97
ProgrammerID23 <76148355+ProgrammerID23@users.noreply.github.com>
2023-03-25 10:47:09 +0700

    Add files via upload

---
+++ untitled/src/Hewan.java
@@ -0,0 +1,17 @@
+public class Hewan {
+    String jenis;
+    String warna;
+
+    public Hewan(String jenis, String warna){
+        this.jenis = jenis;
+        this.warna = warna;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+}
